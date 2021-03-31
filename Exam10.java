e09bb76a69321153566e2b4a35668472b9ffcf6c
Seok-Kyungmin <66109575+Seok-Kyungmin@users.noreply.github.com>
2021-03-31 21:34:11 +0900

    Add files via upload

---
+++ Exam10.java
@@ -0,0 +1,17 @@
+package Step1;
+import java.util.Scanner;
+
+public class Exam10 {
+	
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt(); 
+		System.out.println(A*(B%10));
+		System.out.println(A*(B/10%10));
+		System.out.println(A*(B/10/10));
+		System.out.println(A*B);
+	}
+	
+}
